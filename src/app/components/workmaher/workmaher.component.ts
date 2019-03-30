30315ee16d91c3d8bd39ccd0863edc72dc7e033c
MaherSL <maher.slama@yahoo.fr>
2019-03-30 12:26:25 +0100

    2019.03.30 MSL ExcelService Modif

--- src/app/components/workmaher/workmaher.component.ts
+++ src/app/components/workmaher/workmaher.component.ts
@@ -35,51 +35,10 @@ export class WorkmaherComponent implements OnInit {
   exportAsXLSX(): void {
     this.excelService.exportAsExcelFile(this.data, 'sample');
   }
-
   onFileChange(evt: any) {
-    /* wire up file reader */
-    const target: DataTransfer = <DataTransfer>(evt.target);
-    if (target.files.length !== 1) throw new Error('Cannot use multiple files');
-    const reader: FileReader = new FileReader();
-    reader.onload = (e: any) => {
-      /* read workbook */
-      const bstr: string = e.target.result;
-      const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'binary' });
-
-      /* grab first sheet */
-      const wsname: string = wb.SheetNames[0];
-      const ws: XLSX.WorkSheet = wb.Sheets[wsname];
-
-      /* save data */
-      //this.data = <AOA>(XLSX.utils.sheet_to_json(ws, {header: 1}));
-      this.data2 = (XLSX.utils.sheet_to_json(ws, { header: 1 }));
-      this.alertService.success('Data=' + JSON.stringify(this.data2));
-      var s: string = 'columns2=';
-      var worklinedataList: Tworklinedata[] = [];
-      var worklinedata: Tworklinedata;
-      var line = 0;
-      this.data2.forEach(element => {
-        s += element[1] + ",";
-        line++;
-        worklinedata = new Tworklinedata();
-        worklinedata.c1 = element[0];
-        worklinedata.c2 = element[1];
-        worklinedata.codeworkdata = "id1";
-        worklinedata.lineworkdata = line;
-        worklinedataList.push(worklinedata);
-      });
-      this.alertService.success(s);
-      this.workdataService.saveLines(worklinedataList)
-        .subscribe(
-          data => {
-            this.alertService.success('Enregistrement fait avec succès');
-          },
-          error => {
-            this.alertService.error(JSON.stringify(error));
-          });
-    };
-    reader.readAsBinaryString(target.files[0]);
+    this.excelService.saveSingleFileToWorkLineData(evt, true);
   }
+
 }
 
 
--- src/app/services/excel.service.ts
+++ src/app/services/excel.service.ts
@@ -1,6 +1,9 @@
+import { AlertService } from 'src/app/services/alert.service';
+import { WorkdataService } from 'src/app/services/workdata.service';
 import { Injectable } from '@angular/core';
 import * as FileSaver from 'file-saver';
 import * as XLSX from 'xlsx';
+import { Tworklinedata } from '../entity/Tworklinedata';
 const EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
 const EXCEL_EXTENSION = '.xlsx';
 
@@ -8,7 +11,7 @@ const EXCEL_EXTENSION = '.xlsx';
   providedIn: 'root'
 })
 export class ExcelService {
-constructor() { }
+  constructor(private workdataService: WorkdataService, private alertService: AlertService) { }
   public exportAsExcelFile(json: any[], excelFileName: string): void {
     const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(json);
     const workbook: XLSX.WorkBook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
@@ -16,9 +19,57 @@ constructor() { }
     this.saveAsExcelFile(excelBuffer, excelFileName);
   }
   private saveAsExcelFile(buffer: any, fileName: string): void {
-    const data: Blob = new Blob([buffer], {type: EXCEL_TYPE});
-    FileSaver.saveAs(data, fileName + '_export_' + new  Date().getTime() + EXCEL_EXTENSION);
+    const data: Blob = new Blob([buffer], { type: EXCEL_TYPE });
+    FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
 
-}
+  }
+
+  saveSingleFileToWorkLineData(evt: any, withmessage: boolean) {
+    var data: any[][] = [];
+    /* wire up file reader */
+    const target: DataTransfer = <DataTransfer>(evt.target);
+    if (target.files.length !== 1) throw new Error('Cannot use multiple files');
+    const reader: FileReader = new FileReader();
+    reader.readAsBinaryString(target.files[0]);
+    reader.onload = (e: any) => {
+      /* read workbook */
+      const bstr: string = e.target.result;
+      const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'binary' });
+
+      /* grab first sheet */
+      const wsname: string = wb.SheetNames[0];
+      const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+
+      /* save data */
+      //this.data = <AOA>(XLSX.utils.sheet_to_json(ws, {header: 1}));
+      data = (XLSX.utils.sheet_to_json(ws, { header: 1 }));
+      //this.alertService.success('Data=' + JSON.stringify(this.data2));
+      var s: string = 'columns2=';
+      var worklinedataList: Tworklinedata[] = [];
+      var worklinedata: Tworklinedata;
+      var line = 0;
+      data.forEach(element => {
+        s += element[1] + ",";
+        line++;
+        worklinedata = new Tworklinedata();
+        worklinedata.c1 = element[0];
+        worklinedata.c2 = element[1];
+        worklinedata.codeworkdata = "id1";
+        worklinedata.lineworkdata = line;
+        worklinedataList.push(worklinedata);
+      });
+      //this.alertService.success(s);
+      this.workdataService.saveLines(worklinedataList)
+        .subscribe(
+          data => {
+            if (withmessage)
+              this.alertService.success('Enregistrement fait avec succès');
+          },
+          error => {
+            throw new Error(JSON.stringify(error));
+            //this.alertService.error(JSON.stringify(error));
+          });
+    };
+  }
 }
 
