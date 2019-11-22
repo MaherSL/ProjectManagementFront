a4c036f94fde62001aca18249956068b8bbee4e4
MaherSL <maher.slama@yahoo.fr>
2019-11-22 14:51:23 +0100

    modification lecture excel

--- src/app/services/excel.service.ts
+++ src/app/services/excel.service.ts
@@ -4,6 +4,8 @@ import { Injectable } from '@angular/core';
 import * as FileSaver from 'file-saver';
 import * as XLSX from 'xlsx';
 import { Tworklinedata } from '../entity/Tworklinedata';
+import { Tworkvaluedata } from '../entity/Tworkvaluedata';
+import { Tworkcolumndata } from '../entity/Tworkcolumndata';
 const EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
 const EXCEL_EXTENSION = '.xlsx';
 
@@ -52,6 +54,88 @@ export class ExcelService {
       data.forEach(element => {
         this.alertService.success("dd="+JSON.stringify(element));
         s += element[1] + ",";
+        line++;
+        worklinedata = new Tworklinedata();
+        if (element.length > 10) {
+          nbrcolumns = 10;
+        }
+        else {
+          nbrcolumns = element.length;
+        }
+        for (var i = 0; i < nbrcolumns; i++) {
+          switch (i) {
+            case 0: worklinedata.c1 = element[i]; break;
+            case 1: worklinedata.c2 = element[i]; break;
+            case 2: worklinedata.c3 = element[i]; break;
+            case 3: worklinedata.c4 = element[i]; break;
+            case 4: worklinedata.c5 = element[i]; break;
+            case 5: worklinedata.c6 = element[i]; break;
+            case 6: worklinedata.c7 = element[i]; break;
+            case 7: worklinedata.c8 = element[i]; break;
+            case 8: worklinedata.c9 = element[i]; break;
+            case 9: worklinedata.c10 = element[i]; break;
+            default: worklinedata.c1 = "aaaaaa"; break;
+          }
+        }
+        //        worklinedata.c1 = element[0];
+        //worklinedata.c2 = element[1];
+        worklinedata.codeworkdata = "id2";
+        worklinedata.lineworkdata = line;
+        worklinedataList.push(worklinedata);
+      });
+      //this.alertService.success(s);
+      this.workdataService.deleteAll()
+        .subscribe(
+          data => {
+            this.workdataService.saveLines(null,worklinedataList)
+              .subscribe(
+                data => {
+                  if (withmessage){}
+                    //this.alertService.success('Enregistrement fait avec succèssss');
+                },
+                error => {
+                  throw new Error(JSON.stringify(error));
+                  //this.alertService.error(JSON.stringify(error));
+                });
+          },
+          error => {
+            throw new Error(JSON.stringify(error));
+            //this.alertService.error(JSON.stringify(error));
+          });
+
+
+    };
+  }
+
+  saveSingleFileToWorkValueData(evt: any, withmessage: boolean) {
+    var data: any[][] = [];
+    /* wire up file reader */
+    const target: DataTransfer = <DataTransfer>(evt.target);
+    if (target.files.length !== 1) throw new Error('Cannot use multiple files');
+    const reader: FileReader = new FileReader();
+    reader.readAsBinaryString(target.files[0]);
+    reader.onload = (e: any) => {
+      /* read workbook */
+      const bstr: string = e.target.result;
+      const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'binary' });
+
+      /* grab first sheet */
+      const wsname: string = wb.SheetNames[0];
+      const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+
+      /* save data */
+      //this.data = <AOA>(XLSX.utils.sheet_to_json(ws, {header: 1}));
+      data = (XLSX.utils.sheet_to_json(ws, { header: 1 }));
+      //this.alertService.success('Data=' + JSON.stringify(this.data2));
+      
+      var worklinedataList: Tworklinedata[] = [];
+      var worklinedata: Tworklinedata;
+      var workvaluedata: Tworkvaluedata;
+      var workcolumndata: Tworkcolumndata;      
+      var line = 0;
+      var nbrcolumns = 0;
+      data.forEach(element => {
+        this.alertService.success("dd="+JSON.stringify(element));
         line++;
         worklinedata = new Tworklinedata();
         if (element.length > 10) {
